59682b979e9d6d597ff22a99d73f4a383c225b65
pintafacil <pinta.tronik@gmail.com>
2022-12-18 19:04:41 +0000

    My first commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    private UserPasswordHasherInterface $hasher;
 
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+       $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        
+        $user->setEmail('admin@admin.com');
+        $user->setRoles(array('ROLE_ADMIN', 'ROLE_USER'));
+        $password = $this->hasher->hashPassword($user, 'admin');
+        $user->setPassword($password);
+
+        $manager->persist($user);
         $manager->flush();
     }
+
 }
